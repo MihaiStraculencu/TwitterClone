4038a3c2b6f78708a1aa7d697aea54f3df93277d
StraculencuMihai <painesih2o@gmail.com>
2023-03-16 19:56:51 +0200

    Add user hooks

---
+++ src/hooks/useGetCurrentUser.ts
@@ -0,0 +1,27 @@
+import cookie from 'cookiejs'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { auth, db } from '../../firebase'
+
+export const useGetCurrentUser = (): any => {
+  const [user, setUser] = useState<any>()
+  const userEmail = cookie.get('user') as string
+
+  if (!userEmail) {
+    auth.signOut()
+    return null
+  }
+
+  useEffect(() => {
+    const usersCollectionRef = collection(db, 'users')
+    const q = query(usersCollectionRef, where('email', '==', userEmail))
+
+    getDocs(q).then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data())
+      })
+    })
+  }, [])
+
+  return user
+}
