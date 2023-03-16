143eb462c416aa361207ea49967f46fca5946a28
StraculencuMihai <painesih2o@gmail.com>
2023-03-16 20:36:03 +0200

    Update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import CreateTweet from "./pages/CreateTweet";
-import EditTweet from "./pages/EditTweet";
-import Tweets from "./pages/Tweets";
+import { BrowserRouter, redirect, Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import CreateTweet from './pages/CreateTweet'
+import EditTweet from './pages/EditTweet'
+import { Login } from './pages/Login'
+import { Profile } from './pages/Profile'
+import { Signup } from './pages/Signup'
+import Tweets from './pages/Tweets'
 
 const App = () => (
   <div className="bg-gradient-to-r from-blue-800 to-indigo-900">
@@ -11,14 +14,17 @@ const App = () => (
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/edit/:tweetId" element={<EditTweet />} />
-          <Route path="/create" element={<CreateTweet />} />
           <Route path="/" element={<Tweets />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/create" element={<CreateTweet />} />
+          <Route path="/edit/:tweetId" element={<EditTweet />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
     <Footer />
   </div>
-);
+)
 
-export default App;
+export default App
