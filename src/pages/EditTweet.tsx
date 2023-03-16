a5a6e969e95ce0f19c349c7ead7c72b1122743ae
StraculencuMihai <painesih2o@gmail.com>
2023-03-16 20:39:12 +0200

    Add EditTweets

--- src/pages/EditTweet.tsx
+++ src/pages/EditTweet.tsx
@@ -1,50 +1,53 @@
-import { useParams } from "react-router";
-import { doc, updateDoc, getDoc } from "firebase/firestore";
-import { db } from "../../firebase";
-import { useNavigate } from "react-router";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { useState, useEffect } from "react";
+import { useParams } from 'react-router'
+import { doc, updateDoc, getDoc } from 'firebase/firestore'
+import { db } from '../../firebase'
+import { useNavigate } from 'react-router'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { useState, useEffect } from 'react'
 
 export default function EditTweet() {
-  const [input, setInput] = useState("");
-  const [tweet, setTweet] = useState<any>();
-  const tweetId = useParams().tweetId as string;
-  const navigate = useNavigate();
+  const [input, setInput] = useState('')
+  const [tweet, setTweet] = useState<any>()
+  const tweetId = useParams().tweetId as string
+  const navigate = useNavigate()
 
-  const docRef = doc(db, "tweets", tweetId);
+  const docRef = doc(db, 'tweets', tweetId)
 
   useEffect(() => {
     getDoc(docRef).then((docSnap) => {
-      const tweet = docSnap.data();
+      const tweet = docSnap.data()
 
-      setTweet(tweet as any);
-    });
-  }, []);
+      setTweet(tweet as any)
+    })
+  }, [])
 
   const onSubmit = (data: any) => {
-    const tweetRef = doc(db, "tweets", tweetId);
+    const tweetRef = doc(db, 'tweets', tweetId)
 
     updateDoc(tweetRef, {
       body: data.body,
     })
       .then(() => navigate(`/`))
-      .catch((err) => console.log(`tweet creation failed with error: ${err}`));
-  };
+      .catch((err) => console.log(`tweet creation failed with error: ${err}`))
+  }
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm()
 
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center pt-20">
       <div className="px-4 py-2 border rounded-2xl bg-white flex flex-col justify-between">
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="font-montserrat text-lg break-words resize-none w-[500px]">
+          <div className="flex flex-col font-montserrat text-lg break-words resize-none w-[520px]">
+            <div className="p-4 text-gray-400">
+              <div className="border-b-2 w-fit pb-2">Edit tweet:</div>
+            </div>
             {tweet ? (
               <textarea
-                {...register("body", { required: true })}
+                {...register('body', { required: true })}
                 onChange={(e: any) => setInput(e.target.value)}
                 maxLength={380}
                 className="h-[350px] appearance-none bg-transparent border-none w-full text-gray-700 p-5 leading-tight focus:outline-none text-lg verflow-y-scroll no-scrollbar resize-none"
@@ -67,12 +70,12 @@ export default function EditTweet() {
                 className="w-[80px] bg-blue-500 hover:bg-blue-700 hover:border-blue-700 text-sm text-slate-100 py-2 px-2 rounded-2xl font-bold"
                 type="submit"
               >
-                Tweet
+                Edit
               </button>
             </div>
           </div>
         </form>
       </div>
     </div>
-  );
+  )
 }
