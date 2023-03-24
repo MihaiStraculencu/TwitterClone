457f8736d09d34b9204f17746bb24c98b0e46a04
StraculencuMihai <painesih2o@gmail.com>
2023-03-24 11:31:10 +0200

    Add-CreateNewUser-Page

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,25 @@ import CreateTweet from "./pages/CreateTweet";
 import EditTweet from "./pages/EditTweet";
 import { Login } from "./pages/Login";
 import { Profile } from "./pages/Profile";
-import { Signup } from "./pages/Signup";
+import { CreateNewUser } from "./pages/CreateNewUser";
 import Tweets from "./pages/Tweets";
+import ResetPassword from "./pages/ResetPassword";
 
 const App = () => (
-  <div className="bg-gradient-to-r from-cyan-700 to-indigo-500">
+  <div className="bg-gradient-to-tl from-sky-900 via-sky-900 to-indigo-900">
     <BrowserRouter>
       <Navbar />
       <MobileNavbar />
-      <div className="flex justify-center min-h-screen md:pt-32 pt-10 px-4">
+      <div className="flex justify-center min-h-screen md:pt-32 pt-10 px-4 items-start  ">
         <Routes>
           <Route path="/" element={<Tweets />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/createNewUser" element={<CreateNewUser />} />
           <Route path="/create" element={<CreateTweet />} />
           <Route path="/edit/:tweetId" element={<EditTweet />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/resetPassword" element={<ResetPassword />} />
+
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
